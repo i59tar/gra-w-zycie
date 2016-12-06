f085d3a73015cb0dc6094616570fae060b676614
kKapalka <k.kapalka@hotmail.com>
2016-12-06 08:15:34 +0100

    Pierwszy commit

---
+++ gra-w-zycie.c
@@ -0,0 +1,9 @@
+int main(){
+int tab[2048],rand=(int)&rand,i=-1;
+while((i++)^2048)i[tab]=(rand=(rand*0x41C64E6D+12345)&0x7fffffff)&1024?1<<5:0;
+while(usleep('d'<<'\n'),i+=~i){
+while((i++)^8192){
+if(tab[((i>>2)+2048+"\x01?@A"[i%4])%04000]&1<<5)*(tab+(i>>2))=-~tab[i>>2];
+if(tab[((i>>2)+04000-"\x01?@A"[i%4])%2048]&1<<5)*(tab+(i>>2))=-~tab[i>>2];
+if(i%4==3)tab[i>>2]|=tab[i>>2]^'\"'&&tab[i>>2]^'#'?((tab[i>>2]^3)?0:1<<4):1<<4;};
+while((i++)^10240)if(putchar((tab[i%2048]=tab[i%2048]<<1&1<<5)?'X':' '),!(63^i%0100))putchar(10);}};
